03c9528256909ee05f2595e6fa047db6f7e30f1b
Jordan Willis <jordanwillis@Hal12k.lan1>
2019-06-10 00:03:21 -0700

    I think it's working for python 3+

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-#!/usr/bin env python
-from distutils.core import setup
+#!/usr/bin/env python
 
-setup(name='ANT',
-      version='0.1',
+from distutils.core import setup
+import os
+home = os.path.expanduser('~')
+config = os.path.join(home,'.config/ant_config')
+setup(
+      name='pyrosetta_helper',
       description='Ambiguous Nucleotide Tool (ANT), software for generating and evaluating degenerate codons for natural and expanded genetic codes.',
       author='Martin Engqvist',
       author_email='martin_engqvist@hotmail.com',
-      py_modules=['dna','protein','ANT','base_class','pyperclip','colcol'],
-      scripts=['ANT.py','ANT_GUI.py'])
+      version='0.2',
+      data_files=[(config,['settings.txt'])],
+      packages=['ANT'])
